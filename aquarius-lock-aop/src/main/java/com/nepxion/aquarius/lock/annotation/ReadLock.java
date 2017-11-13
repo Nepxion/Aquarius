245c495eba9ce8066ea6f91962e825c833b48237
Nepxion <1394997@qq.com>
2017-11-13 13:33:31 +0800

    更新注解

--- aquarius-lock-aop/src/main/java/com/nepxion/aquarius/lock/annotation/ReadLock.java
+++ aquarius-lock-aop/src/main/java/com/nepxion/aquarius/lock/annotation/ReadLock.java
@@ -10,15 +10,17 @@ package com.nepxion.aquarius.lock.annotation;
  * @version 1.0
  */
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Inherited
 @Target({ ElementType.METHOD, ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
 public @interface ReadLock {
     /**
      * 锁的key
