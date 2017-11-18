61ec9de815e1e0dac23454cdf618102661544e0b
Nepxion <1394997@qq.com>
2017-11-18 23:58:43 +0800

    修改测试用例

--- aquarius-test/src/main/java/com/nepxion/aquarius/lock/service/MyService2Impl.java
+++ aquarius-test/src/main/java/com/nepxion/aquarius/lock/service/MyService2Impl.java
@@ -25,7 +25,7 @@ public class MyService2Impl {
     @Lock(name = "lock", key = "#id1 + \"-\" + #id2", leaseTime = 5000L, waitTime = 60000L, async = false, fair = false)
     public String doC(String id1, String id2) {
         try {
-            TimeUnit.MILLISECONDS.sleep(4000L);
+            TimeUnit.MILLISECONDS.sleep(2000L);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
