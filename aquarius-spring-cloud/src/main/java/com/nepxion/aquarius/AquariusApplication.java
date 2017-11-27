250df68e47e2496dcb1c1dd9301a1de158abfce5
Nepxion <1394997@qq.com>
2017-11-27 12:39:47 +0800

    暂时去掉@EnableDiscoveryClient

--- aquarius-spring-cloud/src/main/java/com/nepxion/aquarius/AquariusApplication.java
+++ aquarius-spring-cloud/src/main/java/com/nepxion/aquarius/AquariusApplication.java
@@ -13,10 +13,9 @@ package com.nepxion.aquarius;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+// @EnableDiscoveryClient
 @EnableAutoConfiguration
 public class AquariusApplication {
     public static void main(String[] args) {
