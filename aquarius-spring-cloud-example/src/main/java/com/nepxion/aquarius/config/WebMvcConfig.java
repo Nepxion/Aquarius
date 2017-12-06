6c3f3d4b89a62ba2649403a9f1cef64b4a6ca6a7
Nepxion <1394997@qq.com>
2017-12-06 11:37:03 +0800

    解决跨域问题

---
+++ aquarius-spring-cloud-example/src/main/java/com/nepxion/aquarius/config/WebMvcConfig.java
@@ -0,0 +1,26 @@
+package com.nepxion.aquarius.config;
+
+/**
+ * <p>Title: Nepxion Aquarius</p>
+ * <p>Description: Nepxion Aquarius</p>
+ * <p>Copyright: Copyright (c) 2017</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @email 1394997@qq.com
+ * @version 1.0
+ */
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowedOrigins("*");
+    }
+}
