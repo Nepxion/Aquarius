62067016a9835c7658b2927e65da084d3024faca
Nepxion <1394997@qq.com>
2017-12-23 15:43:17 +0800

    修复配置文件中文编码问题

--- aquarius-common/src/main/java/com/nepxion/aquarius/common/constant/AquariusConstant.java
+++ aquarius-common/src/main/java/com/nepxion/aquarius/common/constant/AquariusConstant.java
@@ -13,6 +13,7 @@ package com.nepxion.aquarius.common.constant;
 public class AquariusConstant {
     public static final String ENCODING_UTF_8 = "UTF-8";
     public static final String ENCODING_GBK = "GBK";
+    public static final String ENCODING_ISO_8859_1 = "ISO-8859-1";
 
     public static final String PREFIX = "prefix";
 
