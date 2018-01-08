e5b94958ca7377bba547bd4ae8b2e64be07dd175
Nepxion <1394997@qq.com>
2018-01-08 22:17:27 +0800

    增加本地限流

---
+++ aquarius-limit-local/src/main/java/com/nepxion/aquarius/limit/local/impl/LocalLimitDelegateImpl.java
@@ -0,0 +1,33 @@
+package com.nepxion.aquarius.limit.local.impl;
+
+/**
+ * <p>Title: Nepxion Aquarius</p>
+ * <p>Description: Nepxion Aquarius</p>
+ * <p>Copyright: Copyright (c) 2017</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @email 1394997@qq.com
+ * @version 1.0
+ */
+
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.nepxion.aquarius.common.exception.AquariusException;
+import com.nepxion.aquarius.limit.LimitExecutor;
+import com.nepxion.aquarius.limit.LimitDelegate;
+
+public class LocalLimitDelegateImpl implements LimitDelegate {
+    @Autowired
+    private LimitExecutor limitExecutor;
+
+    @Override
+    public Object invoke(MethodInvocation invocation, String key, int limitPeriod, int limitCount) throws Throwable {
+        boolean status = limitExecutor.tryAccess(key, limitPeriod, limitCount);
+        if (status) {
+            return invocation.proceed();
+        } else {
+            throw new AquariusException("Reach max limited access count=" + limitCount + " within period=" + limitPeriod + " seconds");
+        }
+    }
+}
