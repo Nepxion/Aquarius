bca24a2bca4c6885eb9ae2b064ce0c219acc68a9
Nepxion <1394997@qq.com>
2018-01-11 04:10:09 +0800

    修改脚本

--- deploy.bat
+++ deploy.bat
@@ -12,7 +12,7 @@
 @echo.
 @echo off
 
-@title Nepxion Skeleton
+@title Nepxion Aquarius
 @color 0a
 
 call mvn clean deploy -DskipTests -e -P release -pl aquarius-common,aquarius-common-redis,aquarius-common-zookeeper,aquarius-cache-aop,aquarius-cache-redis,aquarius-id-generator-local,aquarius-id-generator-redis,aquarius-id-generator-zookeeper,aquarius-limit-aop,aquarius-limit-local,aquarius-limit-redis,aquarius-lock-aop,aquarius-lock-local,aquarius-lock-redis,aquarius-lock-zookeeper,aquarius-assembly-cache,aquarius-assembly-id-generator,aquarius-assembly-limit,aquarius-assembly-lock,aquarius-assembly-all -am
