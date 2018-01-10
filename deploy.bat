f6d802b84b58a7bcf8f7895749a816a684b240f4
Nepxion <1394997@qq.com>
2018-01-11 04:02:10 +0800

    修改Pom

--- deploy.bat
+++ deploy.bat
@@ -15,6 +15,8 @@
 @title Nepxion Skeleton
 @color 0a
 
-call mvn clean deploy -DskipTests -e -P release -pl aquarius-common,aquarius-common-redis,aquarius-common-zookeeper,aquarius-cache-aop,aquarius-cache-redis,aquarius-id-generator-local,aquarius-id-generator-redis,aquarius-id-generator-zookeeper,aquarius-limit-aop,aquarius-limit-local,aquarius-limit-redis,aquarius-lock-aop,aquarius-lock-local,aquarius-lock-redis,aquarius-lock-zookeeper,aquarius-assembly-cache,aquarius-assembly-id-generator,aquarius-assembly-limit,aquarius-assembly-lock,aquarius-assembly-all -am
+call mvn clean deploy -DskipTests -e -P release -pl aquarius-cache,aquarius-id-generator,aquarius-limit,aquarius-lock,aquarius-all -am
+
+@rem call mvn clean deploy -DskipTests -e -P release -pl aquarius-common,aquarius-common-redis,aquarius-common-zookeeper,aquarius-cache-aop,aquarius-cache-redis,aquarius-id-generator-local,aquarius-id-generator-redis,aquarius-id-generator-zookeeper,aquarius-limit-aop,aquarius-limit-local,aquarius-limit-redis,aquarius-lock-aop,aquarius-lock-local,aquarius-lock-redis,aquarius-lock-zookeeper -am
 
 pause
