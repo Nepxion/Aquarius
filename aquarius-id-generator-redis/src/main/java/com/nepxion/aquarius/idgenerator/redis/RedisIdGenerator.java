bc299b55d23d2056e21e23eff83cb25856803da1
Nepxion <1394997@qq.com>
2018-01-11 02:01:51 +0800

    重构类结构

--- aquarius-id-generator-redis/src/main/java/com/nepxion/aquarius/idgenerator/redis/RedisIdGenerator.java
+++ aquarius-id-generator-redis/src/main/java/com/nepxion/aquarius/idgenerator/redis/RedisIdGenerator.java
@@ -6,7 +6,6 @@ package com.nepxion.aquarius.idgenerator.redis;
  * <p>Copyright: Copyright (c) 2017-2020</p>
  * <p>Company: Nepxion</p>
  * @author Haojun Ren
- * @email 1394997@qq.com
  * @version 1.0
  */
 
