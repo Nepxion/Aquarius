4a0cdea80bbe29e275f2cf70150e79797355855c
Nepxion <1394997@qq.com>
2018-01-11 05:41:39 +0800

    增加脚本

---
+++ version.bat
@@ -0,0 +1,20 @@
+@echo on
+@echo =============================================================
+@echo $                                                           $
+@echo $                      Nepxion Aquarius                     $
+@echo $                                                           $
+@echo $                                                           $
+@echo $                                                           $
+@echo $  Nepxion Technologies All Right Reserved                  $
+@echo $  Copyright(C) 2017                                        $
+@echo $                                                           $
+@echo =============================================================
+@echo.
+@echo off
+
+@title Nepxion Aquarius
+@color 0a
+
+mvn versions:set -DnewVersion=1.0.1
+
+pause
