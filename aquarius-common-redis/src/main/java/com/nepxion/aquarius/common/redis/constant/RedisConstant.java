0824b1038ce6e3d6d42ce9d94561c3221532444b
Nepxion <1394997@qq.com>
2018-01-12 14:30:23 +0800

    修改COPYRIGHT

--- aquarius-common-redis/src/main/java/com/nepxion/aquarius/common/redis/constant/RedisConstant.java
+++ aquarius-common-redis/src/main/java/com/nepxion/aquarius/common/redis/constant/RedisConstant.java
@@ -3,7 +3,7 @@ package com.nepxion.aquarius.common.redis.constant;
 /**
  * <p>Title: Nepxion Aquarius</p>
  * <p>Description: Nepxion Aquarius</p>
- * <p>Copyright: Copyright (c) 2017-2020</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
  * <p>Company: Nepxion</p>
  * @author Haojun Ren
  * @version 1.0
