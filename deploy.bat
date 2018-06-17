f61be3e954bdadd2f69177110e3ae80e0916c6e1
Nepxion <1394997@qq.com>
2018-06-17 22:31:29 +0800

    修改部署模块

--- deploy.bat
+++ deploy.bat
@@ -15,6 +15,6 @@
 @title Nepxion Aquarius
 @color 0a
 
-call mvn clean deploy -DskipTests -e -P release -pl aquarius-common,aquarius-common-redis,aquarius-common-zookeeper,aquarius-cache-aop,aquarius-cache-redis,aquarius-id-generator-local,aquarius-id-generator-redis,aquarius-id-generator-zookeeper,aquarius-limit-aop,aquarius-limit-local,aquarius-limit-redis,aquarius-lock-aop,aquarius-lock-local,aquarius-lock-redis,aquarius-lock-zookeeper,aquarius-assembly-cache,aquarius-assembly-id-generator,aquarius-assembly-limit,aquarius-assembly-lock,aquarius-assembly-all -am
+call mvn clean deploy -DskipTests -e -P release -pl aquarius-assembly-lock,aquarius-assembly-cache,aquarius-assembly-limit,aquarius-assembly-id-generator,aquarius-assembly-all -am
 
 pause
