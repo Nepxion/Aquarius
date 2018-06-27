5c892e7d464fd67993dc04fe7f0ed33b418a0f9b
Nepxion <1394997@qq.com>
2018-06-27 18:18:31 +0800

    升级版本

--- aquarius-common/src/main/java/com/nepxion/aquarius/common/configuration/AquariusConfiguration.java
+++ aquarius-common/src/main/java/com/nepxion/aquarius/common/configuration/AquariusConfiguration.java
@@ -25,7 +25,7 @@ public class AquariusConfiguration {
         System.out.println("╚╝ ╚╩═╗╠══╩╝╚╩╝╚╩══╩══╝");
         System.out.println("      ║║");
         System.out.println("      ╚╝");
-        System.out.println("Nepxion Aquarius  v1.0.17");
+        System.out.println("Nepxion Aquarius  v1.0.18");
         System.out.println("");
     }
 }
--- version.bat
+++ version.bat
@@ -15,6 +15,6 @@
 @title Nepxion Aquarius
 @color 0a
 
-call mvn versions:set -DnewVersion=1.0.17
+call mvn versions:set -DnewVersion=1.0.18
 
 pause
