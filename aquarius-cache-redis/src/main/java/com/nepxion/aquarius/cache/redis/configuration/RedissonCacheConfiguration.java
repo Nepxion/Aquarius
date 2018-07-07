6501dc2f0c6acb99a0b20a629566632c5a32ba8a
Nepxion <1394997@qq.com>
2018-07-07 16:54:48 +0800

    增加用Redisson缓存的方式

---
+++ aquarius-cache-redis/src/main/java/com/nepxion/aquarius/cache/redis/configuration/RedissonCacheConfiguration.java
@@ -0,0 +1,49 @@
+package com.nepxion.aquarius.cache.redis.configuration;
+
+/**
+ * <p>Title: Nepxion Aquarius</p>
+ * <p>Description: Nepxion Aquarius</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+
+import com.nepxion.aquarius.cache.CacheDelegate;
+import com.nepxion.aquarius.cache.redis.condition.RedisCacheCondition;
+import com.nepxion.aquarius.cache.redis.impl.RedissonCacheDelegateImpl;
+import com.nepxion.aquarius.common.redisson.adapter.RedissonAdapter;
+import com.nepxion.aquarius.common.redisson.constant.RedissonConstant;
+import com.nepxion.aquarius.common.redisson.handler.RedissonHandler;
+import com.nepxion.aquarius.common.redisson.handler.RedissonHandlerImpl;
+
+@Configuration
+public class RedissonCacheConfiguration {
+    @Value("${redisson.config.path:" + RedissonConstant.CONFIG_FILE + "}")
+    private String redissonConfigPath;
+
+    @Autowired(required = false)
+    private RedissonAdapter redissonAdapter;
+
+    @Bean
+    @Conditional(RedisCacheCondition.class)
+    public CacheDelegate redisCacheDelegate() {
+        return new RedissonCacheDelegateImpl();
+    }
+
+    @Bean
+    @Conditional(RedisCacheCondition.class)
+    public RedissonHandler redissonHandler() {
+        if (redissonAdapter != null) {
+            return redissonAdapter.getRedissonHandler();
+        }
+
+        return new RedissonHandlerImpl(redissonConfigPath);
+    }
+}
