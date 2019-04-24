120aa5a6275a3bbdd371c040b6677de2e2bdd83f
Nepxion <1394997@qq.com>
2019-04-24 12:36:32 +0800

    增加旗标

--- aquarius-common/src/main/java/com/nepxion/aquarius/common/configuration/AquariusConfiguration.java
+++ aquarius-common/src/main/java/com/nepxion/aquarius/common/configuration/AquariusConfiguration.java
@@ -12,12 +12,19 @@ package com.nepxion.aquarius.common.configuration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import com.nepxion.aquarius.common.constant.AquariusConstant;
+import com.nepxion.banner.BannerConstant;
+import com.nepxion.banner.Description;
+import com.nepxion.banner.LogoBanner;
+import com.nepxion.banner.NepxionBanner;
+import com.taobao.text.Color;
+
 @Configuration
 @ComponentScan(basePackages = { "com.nepxion.aquarius.common.context" })
 public class AquariusConfiguration {
     static {
-        String logoShown = System.getProperty("nepxion.logo.shown", "true");
-        if (Boolean.valueOf(logoShown)) {
+        /*String bannerShown = System.getProperty(BannerConstant.BANNER_SHOWN, "true");
+        if (Boolean.valueOf(bannerShown)) {
             System.out.println("");
             System.out.println("╔═══╗");
             System.out.println("║╔═╗║");
@@ -27,8 +34,12 @@ public class AquariusConfiguration {
             System.out.println("╚╝ ╚╩═╗╠══╩╝╚╩╝╚╩══╩══╝");
             System.out.println("      ║║");
             System.out.println("      ╚╝");
-            System.out.println("Nepxion Aquarius  v2.0.8");
+            System.out.println("Nepxion Aquarius  v" + AquariusConstant.AQUARIUS_VERSION);
             System.out.println("");
-        }
+        }*/
+
+        LogoBanner logoBanner = new LogoBanner(AquariusConfiguration.class, "/com/nepxion/aquarius/resource/logo.txt", "Welcome to Nepxion", 8, 5, new Color[] { Color.red, Color.green, Color.cyan, Color.blue, Color.yellow, Color.magenta, Color.red, Color.green }, true);
+
+        NepxionBanner.show(logoBanner, new Description(BannerConstant.VERSION + ":", AquariusConstant.AQUARIUS_VERSION, 0, 1), new Description(BannerConstant.GITHUB + ":", BannerConstant.NEPXION_GITHUB + "/Aquarius", 0, 1));
     }
 }
--- aquarius-common/src/main/java/com/nepxion/aquarius/common/constant/AquariusConstant.java
+++ aquarius-common/src/main/java/com/nepxion/aquarius/common/constant/AquariusConstant.java
@@ -10,6 +10,8 @@ package com.nepxion.aquarius.common.constant;
  */
 
 public class AquariusConstant {
+    public static final String AQUARIUS_VERSION = "2.0.8";
+
     public static final String ENCODING_UTF_8 = "UTF-8";
     public static final String ENCODING_GBK = "GBK";
     public static final String ENCODING_ISO_8859_1 = "ISO-8859-1";
